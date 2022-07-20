4604775cfe788523653bbd5357a11c0d36fdbee4
MrRoy121 <nilashishroyjoy@gmail.com>
2022-07-20 11:59:14 +0600

    Responsive_&_Adaptive_User_Interfaces_and_Apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,205 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:udemy_flutter_course/result.dart';
+import 'package:flutter/services.dart';
+import 'package:guide_flutter/widgets/chart.dart';
+import 'package:guide_flutter/widgets/newTransaction.dart';
+import 'package:guide_flutter/widgets/transaction_list.dart';
 
-
-import 'answer.dart';
+import 'model/transaction.dart';
 
 void main() {
+  //WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expenses',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.amber,
+        fontFamily: "Quicksand",
+        appBarTheme: AppBarTheme(
+          toolbarTextStyle: ThemeData.light()
+              .textTheme
+              .copyWith(
+                headline6: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
+              )
+              .bodyText2,
+          titleTextStyle: ThemeData.light()
+              .textTheme
+              .copyWith(
+                headline6: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold),
+              )
+              .headline6,
+        ),
+      ),
+      home: MyHomePage(),
+    );
   }
 }
 
-class _MyAppState extends State<MyApp> {
-  final _question = const [
-    {
-      'question': "What's is your favourite color?",
-      'answer': [
-        {'text': 'Red', 'score': 10},
-        {'text': 'Green', 'score': 2},
-        {'text': 'Blue', 'score': 6},
-        {'text': 'Pink', 'score': 4}
-      ]
-    },
-    {
-      'question': "What's is your favourite animal?",
-      'answer': [
-        {'text': 'Dog', 'score': 4},
-        {'text': 'Cat', 'score': 6},
-        {'text': 'Fish', 'score': 10},
-        {'text': 'Tiger', 'score': 10}
-      ]
-    },
-    {
-      'question': "What's is your favourite Item?",
-      'answer': [
-        {'text': 'Mobile', 'score': 2},
-        {'text': 'Tab', 'score': 10},
-        {'text': 'Laptop', 'score': 8},
-        {'text': 'Computer', 'score': 4}
-      ]
-    }
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _transaction = [
+    //Transaction(id: "i1", title: "Biscuit", amount: 9.88, date: DateTime.now()),
+    //Transaction(id: "i2", title: "Mango", amount: 5.58, date: DateTime.now()),
   ];
-  var _questionIndex = 0;
-  var _totalScore = 0;
+  bool _switchVal = false;
 
-  void _resetQuiz(){
-    setState((){
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-  void _answerQuestion(int score) {
-    _totalScore += score;
+  void _addTransaction(String title, double amount, DateTime date) {
     setState(() {
-      _questionIndex += 1;
+      _transaction.add(new Transaction(
+          id: DateTime.now().toString(),
+          title: title,
+          amount: amount,
+          date: date));
     });
   }
 
+  void _deleteTransaction(String id) {
+    setState(() {
+      _transaction.removeWhere((element) => element.id == id);
+    });
+  }
+
+  void _addTransactionWindow(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (bcx) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(_addTransaction),
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
+  List<Transaction> get _recentTransaction {
+    return _transaction.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("App Bar"),
+    final mq = MediaQuery.of(context);
+    final isLandscape = mq.orientation == Orientation.landscape;
+    final PreferredSizeWidget appbars = Platform.isIOS
+        ? CupertinoNavigationBar(
+            middle: Text(
+              "Expenses",
+              style: TextStyle(color: Colors.black),
+            ),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                GestureDetector(
+                  onTap: () => _addTransactionWindow(context),
+                  child: Icon(CupertinoIcons.add),
+                )
+              ],
+            ),
+          )
+        : AppBar(
+            actions: [
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: () => _addTransactionWindow(context),
+              ),
+            ],
+            title: Text(
+              "Expenses",
+              style: TextStyle(color: Colors.black),
+            ),
+          );
+    final txList = Container(
+        height:
+            (mq.size.height - appbars.preferredSize.height - mq.padding.top) *
+                0.6,
+        child: TransactionList(_transaction, _deleteTransaction));
+    final bodys = SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            if (isLandscape)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Show Chart",
+                    style: Theme.of(context).textTheme.headline5,
+                  ),
+                  Switch.adaptive(
+                      activeColor: Theme.of(context).primaryColor,
+                      value: _switchVal,
+                      onChanged: (val) {
+                        setState(() {
+                          _switchVal = val;
+                        });
+                      })
+                ],
+              ),
+            if (!isLandscape)
+              Container(
+                width: double.infinity,
+                height: (mq.size.height -
+                        appbars.preferredSize.height -
+                        mq.padding.top) *
+                    0.35,
+                child: Chart(_recentTransaction),
+              ),
+            txList,
+            if (isLandscape)
+              _switchVal
+                  ? Container(
+                      width: double.infinity,
+                      height: (mq.size.height -
+                              appbars.preferredSize.height -
+                              mq.padding.top) *
+                          0.7,
+                      child: Chart(_recentTransaction),
+                    )
+                  : txList,
+          ],
         ),
-        body: _questionIndex < _question.length
-            ? Quiz(
-                questionIndex: _questionIndex,
-                answerQuestion: _answerQuestion,
-                question: _question,
-              )
-            : Result(_totalScore, _resetQuiz),
       ),
     );
+    return Platform.isIOS
+        ? CupertinoPageScaffold(
+            child: bodys,
+            navigationBar: appbars,
+          )
+        : Scaffold(
+            appBar: appbars,
+            body: bodys,
+            floatingActionButtonLocation:
+                FloatingActionButtonLocation.centerFloat,
+            floatingActionButton: Platform.isIOS
+                ? Container()
+                : FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: () => _addTransactionWindow(context),
+                  ),
+          );
   }
 }
---
+++ lib/widgets/chart.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:guide_flutter/widgets/chartItem.dart';
+import 'package:intl/intl.dart';
+
+import '../model/transaction.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransaction {
+    return List.generate(7, (index) {
+      final weekday = DateTime.now().subtract(Duration(days: index));
+      var amount = 0.0;
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekday.day &&
+            recentTransactions[i].date.month == weekday.month &&
+            recentTransactions[i].date.year == weekday.year) {
+           amount += recentTransactions[i].amount;
+        }
+      }
+      return {
+        'day': DateFormat.E().format(weekday).substring(0, 1),
+        'amount': amount
+      };
+    }).reversed.toList();
+  }
+
+  double get totalsum {
+    return groupedTransaction.fold(0.0, (sum, item) {
+      return sum + item['amount'];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        elevation: 6,
+        margin: EdgeInsets.all(20),
+        child: Padding(
+          padding: EdgeInsets.all(10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: groupedTransaction.map((e) {
+              return Flexible(
+                fit: FlexFit.tight,
+                child: ChartItem(e['day'], e['amount'],
+                    totalsum == 0.0 ? 0.0 : (e['amount'] as double) / totalsum),
+              );
+            }).toList(),
+          ),
+        ),
+    );
+  }
+}
